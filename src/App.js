d9fa6f109740a22feb330e209416730bb902f73c
Your Name <your_email@whatever.com>
2021-04-29 17:12:22 +0300

    main fix add scss

--- src/App.js
+++ src/App.js
@@ -8,40 +8,6 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a name='home'
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>jjjj</p>
-        <p>jjjj</p>
-        <p>jjjj</p>
-        <p>jjjj</p>
-        <p>jjjj</p>
-        <p>jjjj</p>
-        <p>jjjj</p>
-        <p>jjjj</p>
-        <p>jjjj</p>
-        <p>jjjj</p>
-        <p>jjjj</p>
-        <p>jjjj</p>
-        <p>jjjj</p>
-        <p>jjjj</p>
-        <p>jjjj</p>
-        <p>jjjj</p>
-        <p>jjjj</p>
-        <p>jjjj</p>
-
-      </header> 
       <Main/>
       <Footer/>
     </div>
--- src/components/Main/main.js
+++ src/components/Main/main.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import './main.css';
+import './main2.css';
 import MainVideo from '../../../src/images/file.mp4'
+import CieloImage from '../../../src/images/TheCieloStory.webp'
+import CollectionImage from '../../../src/images/ViewOurCollections.webp'
+import JoinImage from '../../../src/images/JoinOurCom.webp'
 
 function Main() {
     return (
         <main className='main'>
             <div className='container'>
-
-                {/* <img src={MainVideo} alt=''/>    */}
-                <video  autoPlay muted loop src={MainVideo} type="video/mp4"/>
+                <div className='tiles'>
+                    <div className='tile'>
+                        <h2 className='shop-tile'>Introducing Your New Workout Wardrobe</h2>
+                        <button className='shop-button'>Start Shopping</button>
+                    </div> 
+                    <div className='tile feature'>
+                        <video  autoPlay muted loop src={MainVideo} type="video/mp4"/>
+                    </div>
+                    <div className='tile'>
+                        <img src={CieloImage} alt=''/>
+                    </div>
+                    <div className='tile'>
+                        <img src={CollectionImage} alt=''/>
+                    </div>
+                    <div className='tile'>
+                        <img src={JoinImage} alt=''/>
+                    </div>
+                   
+                </div>
+                {/* <h2 className='latest-heading'>LATEST ARRIVALS</h2> */}
             </div>
                 
         </main>
