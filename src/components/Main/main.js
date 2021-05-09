ef82cf74a289e8a67f8a0b91eb0dd6203244c67f
VolkoB <vladimirkobylchak@gmail.com>
2021-05-09 16:46:06 +0300

    main fix add images

--- src/components/Main/main.js
+++ src/components/Main/main.js
@@ -4,6 +4,10 @@ import MainVideo from '../../../src/images/file.mp4'
 import CieloImage from '../../../src/images/TheCieloStory.webp'
 import CollectionImage from '../../../src/images/ViewOurCollections.webp'
 import JoinImage from '../../../src/images/JoinOurCom.webp'
+import Product1 from '../../../src/images/goods1.webp'
+import Product2 from '../../../src/images/goods2.webp'
+import Product3 from '../../../src/images/goods3.webp'
+import Product4 from '../../../src/images/goods4.webp'
 
 function Main() {
     return (
@@ -28,9 +32,39 @@ function Main() {
                     </div>
                    
                 </div>
-                {/* <h2 className='latest-heading'>LATEST ARRIVALS</h2> */}
+               
             </div>
+            <h2 className='latest-heading'>LATEST ARRIVALS</h2>
+            <div className='product-list'>
                 
+                <div className='product'>
+                    <img className='product-image' src={Product1} alt=''/>
+                    <h3 className='product-name'>Running Top</h3>
+                    <hr className='product-line'></hr>
+                    <h3 className='product-price'>19,00₴</h3>
+                </div>
+
+                <div className='product'>
+                    <img className='product-image' src={Product2} alt=''/>
+                    <h3 className='product-name'>Running Top</h3>
+                    <hr className='product-line'></hr>
+                    <h3 className='product-price'>19,00₴</h3>
+                </div>
+
+                <div className='product'>
+                    <img className='product-image' src={Product3} alt=''/>
+                    <h3 className='product-name'>Running Top</h3>
+                    <hr className='product-line'></hr>
+                    <h3 className='product-price'>19,00₴</h3>
+                </div>
+
+                <div className='product'>
+                    <img className='product-image' src={Product4} alt=''/>
+                    <h3 className='product-name'>Running Top</h3>
+                    <hr className='product-line'></hr>
+                    <h3 className='product-price'>19,00₴</h3>
+                </div>
+            </div>
         </main>
     );
 }
